b03cee7fa8491fe22689302a55090e3c728c4490
ngt-kn <ngt.kn.dev@gmail.com>
2018-07-18 20:24:45 -0700

    Initial Commit

---
+++ CheckListApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckListApp
+//
+//  Created by ngt kn on 7/18/18.
+//  Copyright © 2018 ngtkn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
